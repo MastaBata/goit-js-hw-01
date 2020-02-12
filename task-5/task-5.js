385c2f481f8120d7f373becbd05848f349df8c60
MastaBata <60700819+MastaBata@users.noreply.github.com>
2020-02-12 16:30:21 +0200

    Add files via upload

--- task-5/task-5.js
+++ task-5/task-5.js
@@ -1,33 +1,33 @@
 let print = prompt('Страна:');
-let country = print.toLowerCase();
+let country = print[0].toUpperCase() + print.slice(1).toLowerCase();
 let cash;
 
 switch(country) {
-    case "китай": 
+    case "Китай": 
         cash = 100;
         alert(`Доставка в ${country} будет стоить ${cash} кредитов`);
         
         break
   
-    case "чили": 
+    case "Чили": 
         cash = 250;
         alert(`Доставка в ${country} будет стоить ${cash} кредитов`);
         
         break
 
-    case "австралия": 
+    case "Австралия": 
         cash = 170;
         alert(`Доставка в ${country} будет стоить ${cash} кредитов`);
         
         break
         
-    case "индия": 
+    case "Индия": 
         cash = 80;    
         alert(`Доставка в ${country} будет стоить ${cash} кредитов`);
         
         break
     
-    case "ямайка": 
+    case "Ямайка": 
         cash = 120;    
         alert(`Доставка в ${country} будет стоить ${cash} кредитов`);
        
